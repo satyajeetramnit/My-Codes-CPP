f600fbc9648f612e2092a3216441d5d47e9667f0
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-06 13:12:02 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+int main(){
+//Rectangle Pattern
+    // int r,c;
+    // cout<<"Enter rows and column : "<<endl;
+    // cin>>r>>c;
+    // for(int i=1;i<=r;i++){
+    //     for (int j = 0; j < c; j++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+//Hollow Rectangle Pattern
+    // int r,c;
+    // cout<<"Enter rows and column : "<<endl;
+    // cin>>r>>c;
+    // for(int i=1;i<=r;i++){
+    //     for (int j=1;j<=c;j++)
+    //     {
+    //         if(i==1||j==1||i==r||j==c)
+    //         cout<<"* ";
+    //         else
+    //         cout<<"  ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+//Inverted Half Piramid
+    // int r,c;
+    // cout<<"Enter rows and column : "<<endl;
+    // cin>>r>>c;
+    // for(int i=r;i>0;i--){
+    //     for (int j=0;j<i;j++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<"\n";
+    // }
+    
+//Half piramid after 180 rotation
+    // int n;
+    // cout<<"Enter a no. : "<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         if (j<=n-i)
+    //         {
+    //             cout<<" ";
+    //         }
+    //         else
+    //         {
+    //             cout<<"*";
+    //         }
+    //     }
+    //     cout<<"\n";
+    // }
+
+//Half Piramid using numbers
+    // int n;
+    // cout<<"Enter a no. : "<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         if (j<=i)
+    //         {
+    //             cout<<i;
+    //         }
+    //         else
+    //         {
+    //             cout<<" ";
+    //         }
+            
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+    // int n;
+    // cout<<"Enter a no. : "<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout<<j;
+            
+    //     }
+    //     cout<<"\n";
+    // }
+
+//Floyds Triangle
+    // int n;
+    // cout<<"Enter a no. : "<<endl;
+    // cin>>n;
+    // int a=1;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout<<a++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+//Buterfly Pattern
+    // int n; 
+    // cin >> n; 
+    // for (int i = 1; i <= n; i++) { 
+    //     int space = 2 * n - 2 * i; 
+    //     for(int j = 1; j <= i; j++) { 
+    //         cout << "*"; 
+    //         }
+    //     for (int j = 1; j <= space; j++) { 
+    //         cout << " "; 
+    //         }
+    //     for (int j = 1; j <= i; j++) { 
+    //         cout << "*"; 
+    //         }
+    //         cout << "\n"; 
+    //     }
+    //     for (int i = n; i >= 1; i--) { 
+    //         int space = 2 * n - 2 * i; 
+    //         for (int j = 1; j <= i; j++) { 
+    //             cout << "*";
+    //             }
+    //         for (int j = 1; j <= space; j++) { 
+    //             cout << " "; 
+    //             }
+    //         for (int j = 1; j <= i; j++) { 
+    //             cout << "*"; 
+    //             }
+    //             cout << "\n"; 
+    //         }
+
+//Hollow Butterfly Pattern
+    int n;
+    
+
+    return 0;
+}
