f9f102b432a7f16cde6f2128860ada75be954042
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-08 14:02:41 +0530

    Add files via upload

---
+++ factors.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :"<<endl;
+    cin>>n;
+    cout<<"Factors of "<<n<<" are : "<<endl;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n%i==0)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+    return 0;
+}
