c20cf0587389ceb501f0442f4a216ff446ea69b0
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-09 16:17:39 +0530

    Add files via upload

---
+++ vowelOrConsonant.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter an alphabet : ";
+    cin>>ch;
+    switch (ch)
+    {
+    case 'a':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'e':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'i':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'o':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'u':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'A':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'E':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'I':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'O':
+        cout<<"It is a vowel."<<endl;
+        break;
+    case 'U':
+        cout<<"It is a vowel."<<endl;
+        break;
+    default:
+        cout<<"Nahi Bateyenge ki consonant hai..."<<endl;
+        break;
+    }
+    
+    return 0;
+}
