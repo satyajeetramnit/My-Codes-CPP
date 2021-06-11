19ec0c9369d7262148bc57506d8adb18d28dfc3b
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-11 15:20:22 +0530

    Add files via upload

---
+++ fibonacciFunction.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void fib(int num){
+    int t1=0;
+    int t2=1;
+    int nextTerm;
+    for (int i = 1; i <= num; i++)
+    {
+        cout<<t1<<endl;
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
+    return;
+}
+
+int main(){
+    int n;
+    cout<<"Enter number of terms in fibonacci series to print : ";
+    cin>>n;
+    fib(n);
+    return 0;
+}
