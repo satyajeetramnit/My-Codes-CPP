37521a3278895c9d5d4b7bcf8c0ac50727d9982f
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-22 13:40:43 +0530

    Add files via upload

---
+++ selectionSort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array : "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j]<arr[i])
+            {
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+            
+        }
+        
+    }
+    
+    cout<<"Sorted elements are :"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
