6068a64e2956fb407241b6bda4a420ceb7854a4d
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-25 21:17:35 +0530

    Add files via upload

---
+++ possibleSubarray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array : "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k];
+            }
+            cout<<"\n";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
