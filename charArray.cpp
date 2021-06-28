d55a8d3294b0b98b3c017b3a1758a31ff42ee23d
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-06-29 01:53:42 +0530

    Add files via upload

---
+++ charArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char arr[100]="satyajeet";
+    // int i=0;
+    // while(arr[i] != '\0'){
+    //     cout<<arr[i];
+    //     i++;
+    // }
+
+    // int n;
+    // cout<<"Enter the size of the array"<<endl;
+    // cin>>n;
+    // char arr[n+1];
+    // cout<<"Enter the characters"<<endl;
+    // for(int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i];
+    // }
+
+    char arr[100];
+    cin>>arr;
+    cout<<arr;
+
+    return 0;
+}
